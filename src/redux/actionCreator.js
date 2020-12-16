85f073eeb59f334cee9a7d11658a915111e833a4
antimonysayz <antimonysayz@gmail.com>
2020-12-15 23:17:08 -0500

    adding dispatch for foodpage

--- src/redux/actionCreator.js
+++ src/redux/actionCreator.js
@@ -10,3 +10,16 @@ export const setFoods = () => {
     })
   )}
 }
+
+export const setSelectedFood = (id) => {
+  return dispatch => {
+    fetch(API + "/foods" + id)
+    .then(res => res.json())
+    .then(food => dispatch({
+      type: "SET_SELECTED_FOOD",
+      payload: food
+    }))
+  }
+}
+
+export const unsetRestaurant = () => ({type: "UNSET_RESTAURANT"})
--- src/redux/foodsReducer.js
+++ src/redux/foodsReducer.js
@@ -1,12 +1,22 @@
+const nullObj = {
+    id: null,
+    name: ""
+}
+
+
 const initialState = {
-    foods: []
+    foods: [],
+    selectedFood: nullObj
   }
 
 const foodsReducer = (state=initialState, action) => {
       switch(action.type){
         case 'SET_FOODS':
-          
           return {...state, foods: action.payload}
+        case 'SET_SELECTED_RESTAURANT':
+            return {...state, selectedFood: action.payload }
+        case "UNSET_FOODS":
+            return {...state, selectedFood: nullObj}
         default:
           return {...state}
       }
