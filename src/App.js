cb981182c2305000f3ca1b0891c189b20ece74de
antimonysayz <antimonysayz@gmail.com>
2020-12-26 22:54:43 -0500

    removing foods#index from navbar, addToMeal reducer submits POST request to Meal#ID/Ingredients

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                  <Nav.Link href="/foods">Foods</Nav.Link>
+                  {/* <Nav.Link href="/foods">Foods</Nav.Link> */}
                   <Nav.Link href="/meals">Meals</Nav.Link>
                   <Nav.Link href="/uploads">Image Guesser</Nav.Link>
                   <NavDropdown title="TensorFlow" id="basic-nav-dropdown">
